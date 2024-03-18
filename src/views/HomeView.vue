e97b36aa2011479c028aa496d6e6fa9b1fdd8b0c
KarolisG <kgaivenis@gmail.com>
2024-03-18 15:42:12 +0200

    add init layout structure for phone view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,38 @@
 
 <template>
   <main>
-    <h1>Hello World</h1>
+    <div>
+      <p>placeholder to list all habits for that exact day</p>
+      <h1>Habit list</h1>
+      <ul>
+        <li>Habit #1</li>
+        <li>Habit #2</li>
+        <li>Habit #3</li>
+        <li>Habit #4</li>
+        <li>Habit #5</li>
+      </ul>
+    </div>
+
+    <div class="btn-wrapper">
+      <p>placeholder for add habit btn</p>
+      <button type="button">Add habit</button>
+    </div>
   </main>
 </template>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 80vh;
+  border: 1px solid orange;
+  border-radius: 5px;
+}
+
+.btn-wrapper {
+  border: 1px solid red;
+  border-radius: 5px;
+  margin-top: 2rem;
+}
+</style>
