629df555d68a4fd9c682fcba8da86974bb84db96
KarolisG <kgaivenis@gmail.com>
2024-03-20 10:25:36 +0200

    display recorded habits in daydetails

--- src/views/DayDetails/DayDetails.vue
+++ src/views/DayDetails/DayDetails.vue
@@ -19,8 +19,6 @@ function loadDayData(dayID) {
   } else {
     recordedHabits.value = [];
   }
-  console.log("i've just loaded data for", dayID);
-  console.log('this is data for the day:', recordedHabits.value);
 }
 
 watchEffect(() => {
@@ -32,6 +30,14 @@ watchEffect(() => {
   <main>
     <h1>{{ props.id }}</h1>
     <h3>Daily Habits</h3>
+    <div v-if="recordedHabits.length > 0">
+      <ul>
+        <li v-for="(habit, index) in recordedHabits[0].habits" :key="index">
+          {{ habit.name }} - {{ habit.completed }}
+        </li>
+      </ul>
+    </div>
+    <div v-else>This day has no recorded habits</div>
   </main>
   <div class="btn-wrapper">
     <p>placeholder for add habit btn</p>
@@ -45,7 +51,6 @@ watchEffect(() => {
 main {
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
   height: 80vh;
   border: 1px solid orange;
   border-radius: 5px;
