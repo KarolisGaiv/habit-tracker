08831533b2888b5aa366aeeb8e74b8ba76b6bf10
KarolisG <kgaivenis@gmail.com>
2024-03-23 16:35:21 +0200

    add basic styling to dashboard

--- src/views/Dashboard/DashboardView.vue
+++ src/views/Dashboard/DashboardView.vue
@@ -1,7 +1,36 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+import storageUtility from '../../utils/storageUtility';
+
+const allHabits = ref(storageUtility.getData())
+
+</script>
 
 <template>
-  <div>Dashboard</div>
+  <h1>Your Habit List</h1>
+    <ul>
+      <li class="habit-card" v-for="(habit, index) in allHabits" :key="index">
+        {{ habit.name }}
+      </li>
+    </ul>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+ul {
+  list-style: none;
+  padding: 0;
+  margin: 2rem 0;
+}
+
+.habit-card {
+  background: rgb(0 128 128);
+  border-radius: 5px;
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem;
+  margin: 0.5rem 0;
+}
+
+</style>
--- vite.config.js
+++ vite.config.js
@@ -10,9 +10,5 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  server: {
-    host: 'localhost',
-    port: 8080
   }
 });
