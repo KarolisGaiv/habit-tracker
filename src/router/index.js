3a60b68250922534e9ca1bfdf9c183b5a763fc04
KarolisG <kgaivenis@gmail.com>
2024-03-23 20:29:36 +0200

    add error page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import DayDetails from '../views/DayDetails/DayDetails.vue';
 import AddHabit from '../views/AddHabit/AddHabit.vue';
 import DashboardView from '../views/Dashboard/DashboardView.vue';
+import NotFound from '../views/NotFound/NotFound.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -23,9 +24,16 @@ const router = createRouter({
       component: AddHabit,
       props: true
     },
+    // redirect
     {
       path: "/day/",
       redirect: "/"
+    },
+    // catch 404 (catches everything else not indicated above)
+    {
+      path: "/:catchAll(.*)",
+      name: "NotFound",
+      component: NotFound
     }
     // {
     //   path: '/about',
