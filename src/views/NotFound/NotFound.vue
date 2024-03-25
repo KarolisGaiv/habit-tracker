ae2de8e5f9ac271d0bbe2ad4a1c157798165d6a0
KarolisG <kgaivenis@gmail.com>
2024-03-25 15:35:01 +0200

    fix lint warnings

--- src/views/NotFound/NotFound.vue
+++ src/views/NotFound/NotFound.vue
@@ -1,24 +1,26 @@
 <template>
-    <main>
+  <main>
     <h2>404</h2>
     <h3>Page Not Found</h3>
-    <RouterLink :to="{name: 'Dashboard'}"><button type='button' class='home-btn'>Go Home</button></RouterLink>
-</main>
-  </template>
+    <RouterLink :to="{ name: 'Dashboard' }"
+      ><button type="button" class="home-btn">Go Home</button></RouterLink
+    >
+  </main>
+</template>
 
-  <style scoped>
+<style scoped>
 main {
-    align-items: center;
-    display: flex;
-    flex-direction: column;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
 }
 
 h3 {
-    margin: 1rem;
+  margin: 1rem;
 }
 
 .home-btn {
-  background-color: rgb(38, 186, 38);
+  background-color: rgb(38 186 38);
   color: white;
   font-weight: bold;
   cursor: pointer;
@@ -26,7 +28,6 @@ h3 {
     background-color 0.3s ease,
     border-color 0.3s ease;
   outline: none;
-  padding: 0 1rem;
   border-radius: 5px;
   border: none;
   height: 100%;
@@ -35,6 +36,6 @@ h3 {
 
 .home-btn:focus,
 .home-btn:hover {
-  background-color: rgb(28, 125, 28);
+  background-color: rgb(28 125 28);
 }
 </style>
